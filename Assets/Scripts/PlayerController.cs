7db429ffc045cec0b4b4edd0d5b8877e92f0536b
Simone Di Giorgi <simonetomi@gmail.com>
2017-09-26 19:25:33 +0200

    NavMesh movements, camera rotation, zoom

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))                                            // Movement - LEFT MOUSE
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))                                            // Interaction - RIGHT MOUSE
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // Do stuff
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update () {
+		
+	}
+
+    public void MoveToPoint (Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+}
