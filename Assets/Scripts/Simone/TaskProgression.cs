8d81d555afc61a0f6d9b5e3f8274994ccdbb5a0e
Simone Di Giorgi <simonetomi@gmail.com>
2017-10-18 12:03:03 +0200

    Progress bar on Main

---
+++ Assets/Scripts/Simone/TaskProgression.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskProgression : MonoBehaviour {
+
+    public Transform LoadingBar;
+    public Transform TextIndicator;
+    //public Transform TextLoading;
+
+    [SerializeField] private float currentAmont;
+    [SerializeField] private float speed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if(currentAmont < 100)
+        {
+            currentAmont += speed * Time.deltaTime;
+            TextIndicator.GetComponent<Text>().text = ((int)currentAmont).ToString() + "%";
+            //TextLoading.gameObject.SetActive(true);
+        }
+        else
+        {
+            //TextLoading.gameObject.SetActive(false);
+            TextIndicator.GetComponent<Text>().text = "DONE";
+        }
+        LoadingBar.GetComponent<Image>().fillAmount = currentAmont / 100;
+		
+	}
+}
