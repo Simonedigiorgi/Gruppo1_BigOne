ba4df8e050abff7365180000abec3833fb32d8c8
Stefano Mauri <dbga_42@dbga.local>
2017-10-19 13:26:26 +0200

    Update Main Scena

---
+++ Assets/Scripts/Stefano/AttivaUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttivaUI : ButtonStyle {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		/*if()
+		{
+
+			GoAnimation ("MainMenu");
+
+		}*/
+		
+	}
+}
