8743edc8d2eb101f3a74de1f7c7e6e8dd3d46633
Vincenzo Villani <v.villani84@alice.it>
2017-10-26 17:18:14 +0200

    Updated Camera Movement and Camera Change

---
+++ Assets/Scripts/Vincenzo/BuildingCamera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingCamera : CameraBehaviour {
+
+    void Start()
+    {
+        cameraType = 1;    
+    }
+
+    public override void GetCameraBeahviour()
+    {
+         
+    }
+
+}
